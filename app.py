4f3c564b13c43b8ffbb990285c58df158515f695
Tim "Second Wind" Humphrey <tchumphrey1965@gmail.com>
2022-02-04 15:21:52 -0500

    Updated Jupyter  App, Scraping and Index

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_pymongo import PyMongo
 import scraping
 
 app = Flask(__name__)
+
 # Use flask_pymongo to set up mongo connection
 app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
 mongo = PyMongo(app)
@@ -18,6 +19,7 @@ def scrape():
    mars_data = scraping.scrape_all()
    mars.update_one({}, {"$set":mars_data}, upsert=True)
    return redirect('/', code=302)
+
 
 if __name__ == "__main__":
    app.run()
--- scraping.py
+++ scraping.py
@@ -54,50 +54,84 @@ def mars_news(browser):
 
     return news_title, news_p
 
+###JPL Space Images Featured Image####
+##DISPLAYING FEATURED IMAGE
 
 def featured_image(browser):
     # Visit URL
-    url = 'https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/index.html'
+    url = 'https://spaceimages-mars.com'
     browser.visit(url)
 
     # Find and click the full image button
     full_image_elem = browser.find_by_tag('button')[1]
     full_image_elem.click()
 
+    
     # Parse the resulting html with soup
     html = browser.html
     img_soup = soup(html, 'html.parser')
+    #img_soup will print fancybox-image html description
 
     # Add try/except for error handling
     try:
         # Find the relative image url
         img_url_rel = img_soup.find('img', class_='fancybox-image').get('src')
-
+        #img_url_rel will print 'image/featured/mars3.jpg'
     except AttributeError:
         return None
 
     # Use the base url to create an absolute url
-    img_url = f'https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/{img_url_rel}'
-
+    img_url = f'https://spaceimages-mars.com/{img_url_rel}'
+    #img_url will print 'https://spaceimages-mars.com/image/featured/mars3.jpg'
     return img_url
+    
+    #END OF RETURNINNG ONE IMAGE
+
+    ##MARS FACTS
+    #TABLES 1 & 2
 
 def mars_facts():
-    # Add try/except for error handling
-    try:
-        # Use 'read_html' to scrape the facts table into a dataframe
-        df = pd.read_html('https://data-class-mars-facts.s3.amazonaws.com/Mars_Facts/index.html')[0]
 
+    try:
+        df = pd.read_html('https://galaxyfacts-mars.com')[0]
+        # dr.head will print table from [0,2] rows 0 - 4
     except BaseException:
         return None
 
-    # Assign columns and set index of dataframe
+        # Assign columns and set index of dataframe
     df.columns=['Description', 'Mars', 'Earth']
     df.set_index('Description', inplace=True)
+        #df will print table with formatted descriptions
 
-    # Convert dataframe into HTML format, add bootstrap
-    return df.to_html(classes="table table-striped")
+        # Convert dataframe into HTML format, add bootstrap
+    return df.to_html()
+        ##End of tables
+
+## HEMISPHERES 
+def hemisphere(browser):
+    # 1. Use browser to visit the URL
+    url = "https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars"
+    browser.visit(url)
+
+    # 2. Create a list to hold the images and titles.
+    hemisphere_image_urls = []
+    #imgs_links= browser.find_by_css("a.product-item h3")
+
+    # 3. Write code to retrieve the image urls and titles for each hemisphere.
+    for i in range(4):
+    #create empty dictionary
+        hemispheres = {}
+        browser.find_by_css('a.product-item h3')[i].click()
+        element = browser.find_link_by_text('Sample').first
+        img_url = element['href']
+        title = browser.find_by_css("h2.title").text
+        hemispheres["img_url"] = img_url
+        hemispheres["title"] = title
+        hemisphere_image_urls.append(hemispheres)
+        browser.back()    
+    return hemisphere_image_urls
+##END of SCRAPE ALL
 
 if __name__ == "__main__":
-
     # If running as script, print scraped data
     print(scrape_all())
